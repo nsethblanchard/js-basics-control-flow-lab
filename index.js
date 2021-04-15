1738a0c204b9129c7942671f0e83a91c9a11372a
Seth Blanchard <nsethblanchard@gmail.com>
2021-04-15 16:19:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(miles){
+  if (miles <= 400){
+    return 'This one is on me!';
+  } else if ( miles > 2000 && miles < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city == "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === "generous"){
+    return 'Thank you so much.';
+  } else if ( tip === "not as generous") {
+    return 'Thank you.'
+  } else {
+    return 'Bye.'
+  }
 }
